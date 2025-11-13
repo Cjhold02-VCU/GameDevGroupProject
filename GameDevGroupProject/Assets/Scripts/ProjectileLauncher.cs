c45bedeb12dc129aa97d2bd5be636c3ae52ce588
cuppsos <cuppsos@vcu.edu>
2025-11-13 17:55:09 -0500

    Finished ProjectileLauncher
    Basic projectile launcher. It can be modified to fit any gun we need.

---
+++ GameDevGroupProject/Assets/Scripts/ProjectileLauncher.cs
@@ -0,0 +1,155 @@
+using UnityEngine;
+using TMPro;
+using System.Diagnostics.CodeAnalysis;
+using UnityEngine.UIElements;
+
+
+public class ProjectileLauncher : MonoBehaviour
+{
+
+    // Bullet object
+    public GameObject bullet;
+
+    // Bullet force
+    public float shootForce, upwardForce;
+
+    // Gun stats
+    public float timeBetweenShooting, spread, reloadTime, timeBetweenShots;
+    public int magazineSize, bulletsPerTap;
+    public bool allowButtonHold;
+
+    int bulletsLeft, bulletsShot;
+
+    // bools
+    bool shooting, readyToShoot, reloading;
+
+    // References
+    public Camera fpsCam;
+    public Transform attackPoint;
+
+    // Graphics
+    public GameObject muzzleFlash;
+    public TextMeshProUGUI ammunitionDisplay;
+
+    // BUG FIXING
+    public bool allowInvoke = true;
+
+    private void Awake()
+    {
+        // Make sure the magazine is full
+        bulletsLeft = magazineSize;
+        readyToShoot = true;
+    }
+
+    private void Update()
+    {
+        MyInput();
+
+        // Set ammo display, if it exists
+        if (ammunitionDisplay != null)
+            ammunitionDisplay.SetText(bulletsLeft / bulletsPerTap + " / " + magazineSize / bulletsPerTap);
+
+    }
+
+    private void MyInput()
+    {   
+        // Check if allowed to hold down button and take corresponding input
+        if (allowButtonHold) shooting = Input.GetKey(KeyCode.Mouse0);
+        else shooting = Input.GetKeyDown(KeyCode.Mouse0);
+
+        // Reloading
+        // If press R and bullets left is less than mag size and not currently reloading
+        if (Input.GetKeyDown(KeyCode.R) && bulletsLeft < magazineSize && !reloading) 
+            Reload();
+
+        // Reload automatically when trying to shoot without ammo
+        // if (readyToShoot && shooting && !reloading && bulletsLeft <= 0) Reload;
+        // Could be left out for hardcore element
+
+        // If readyToShoot and shooting and not reloading and bullets left > 0
+        if (readyToShoot && shooting && !reloading && bulletsLeft > 0)
+        {
+            bulletsShot = 0;
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        readyToShoot = false;
+
+        
+
+        // Find the exact hit position
+        Ray ray = fpsCam.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+        RaycastHit hit;
+
+        // Check if ray hits target
+        Vector3 targetPoint;
+        if (Physics.Raycast(ray, out hit))
+            targetPoint = hit.point;
+        else
+            targetPoint = ray.GetPoint(75);
+
+        // Calculate direction from attackpoint to targetPoint
+        // Formula for a vector from point A to point B is always A - B
+        Vector3 directionWithoutSpread = targetPoint - attackPoint.position;
+
+        // calculate spread
+        float x = Random.Range(-spread, spread);
+        float y = Random.Range(-spread, spread);
+
+        // Calculate new direction with spread
+        // Just add spread as x and y component to direction vector
+        Vector3 directionWithSpread = directionWithoutSpread + new Vector3(x, y, 0);
+
+        // Instantiate bullet/projectile
+        GameObject currentBullet = Instantiate(bullet, attackPoint.position, Quaternion.identity);
+        // Rotate bullet to point towards the direction its shooting
+        currentBullet.transform.forward = directionWithSpread.normalized;
+
+        // Add forces to the bullet
+        // GameObject must have RigidBody component, use .AddForce()
+        currentBullet.GetComponent<Rigidbody>().AddForce(directionWithSpread.normalized * shootForce, ForceMode.Impulse);
+        // Code for upward force on projectile
+        // currentBullet.GetComponent<Rigidbody>().AddForce(fpsCam.transform.up * upwardForce, ForceMode.Impulse);
+
+        // Instantiate Muzzle Flash, if there is one
+        if (muzzleFlash != null)
+            Instantiate(muzzleFlash, attackPoint.position, Quaternion.identity);
+
+        bulletsLeft--;
+        bulletsShot++;
+
+        // Invoke resetShot function (if not already invoked)
+        if (allowInvoke)
+        {
+            Invoke("Reset Shot", timeBetweenShooting);
+            allowInvoke = false;
+        }
+
+        // If more than one bulletsPerTap make sure to repeat shoot function
+        if (bulletsShot < bulletsPerTap && bulletsLeft > 0)
+            Invoke("Shoot", timeBetweenShots);
+    }
+
+
+    private void ResetShot()
+    {
+        // Allow shooting and invoking again
+        readyToShoot = true;
+        allowInvoke = true;
+    }
+
+    private void Reload()
+    {
+        reloading = true;
+        Invoke("ReloadFinished", reloadTime);
+    }
+
+    private void ReloadFinish()
+    {
+        bulletsLeft = magazineSize;
+        reloading = false;
+    }
+}
